2462bee38c517d13d8517a19fd4f4f1b436a5767
Dmitriy3388 <136285635+Dmitriy3388@users.noreply.github.com>
2025-07-27 09:47:44 +0300

    мейн дошбоард

--- warehouse/account/urls.py
+++ warehouse/account/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'account:'
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
     #адреса входа и выхода
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='account:login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='account:logout'),
     # change password urls
     path('', include('django.contrib.auth.urls')),
     #path('', views.dashboard, name='dashboard'),
