f8379cd6c6314389ef3e12ee9495285616634c2f
Dmitriy3388 <136285635+Dmitriy3388@users.noreply.github.com>
2025-10-09 21:57:31 +0300

    Редирект профиля

--- warehouse/account/views.py
+++ warehouse/account/views.py
@@ -69,6 +69,7 @@ def edit(request):
             profile_form.save()
             messages.success(request, 'Profile updated '\
                                       'successfully')
+            return redirect('account:profile')
         else:
             messages.error(request, 'Error updating your profile')
     else:
