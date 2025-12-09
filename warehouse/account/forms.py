8212a2f5fc58da3172da9819c013f1fd0a1823b7
Dmitriy3388 <136285635+Dmitriy3388@users.noreply.github.com>
2025-12-09 22:19:09 +0300

    Исправлена регистрация

--- warehouse/account/forms.py
+++ warehouse/account/forms.py
@@ -6,28 +6,75 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from .models import Profile
+
+
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password',
-                                widget=forms.PasswordInput)
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Повторите пароль',
+        widget=forms.PasswordInput
+    )
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
+        labels = {
+            'username': 'Имя пользователя',
+            'first_name': 'Имя',
+            'email': 'Email'
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
+        password = cd.get('password')
+        password2 = cd.get('password2')
+
+        # Если пароль есть - валидируем его
+        if password:
+            try:
+                validate_password(password)
+            except ValidationError as e:
+                # Переводим сообщения об ошибках на русский
+                russian_errors = []
+                for error in e.messages:
+                    if 'too short' in error.lower():
+                        russian_errors.append('Пароль должен содержать минимум 8 символов.')
+                    elif 'too common' in error.lower():
+                        russian_errors.append('Этот пароль слишком распространен.')
+                    elif 'entirely numeric' in error.lower():
+                        russian_errors.append('Пароль не может состоять только из цифр.')
+                    elif 'too similar' in error.lower():
+                        russian_errors.append('Пароль слишком похож на другую личную информацию.')
+                    else:
+                        russian_errors.append(error)  # Оставляем оригинальное сообщение
+
+                raise forms.ValidationError(russian_errors)
+
+        # Проверяем совпадение паролей
+        if password and password2 and password != password2:
+            raise forms.ValidationError('Пароли не совпадают.')
+
+        return password2
 
     def clean_email(self):
         data = self.cleaned_data['email']
         if User.objects.filter(email=data).exists():
-            raise forms.ValidationError('Email already in use.')
+            raise forms.ValidationError('Email уже используется.')
         return data
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if username and User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Имя пользователя уже занято.')
+        return username
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
